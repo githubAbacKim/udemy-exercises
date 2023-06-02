b17776c97574dcf5cd31ccef7c7c75354d129870
Christian Kim Abac <abackim9087@gmail.com>
2023-06-02 22:04:13 +0800

    update

---
+++ javascript/special-array-methods/prototype.js
@@ -0,0 +1,12 @@
+function User(email,name){
+    this.email = email;
+    this.name = name;
+    this.online = false;
+}
+
+User.prototype.login = function(){
+    this.online = true;
+    console.log(this.email,'has logged in');
+}
+
+User.prototype.logout
