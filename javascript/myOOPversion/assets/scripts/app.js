5ec1f24033f58e063e33aa32f8acfea288e2e6d0
Christian Kim Abac <abackim9087@gmail.com>
2023-07-31 21:57:30 +0800

    update

---
+++ javascript/myOOPversion/assets/scripts/app.js
@@ -0,0 +1,179 @@
+class Templating{
+    constructor(container,template,data){
+        this.container = container;
+        this.template = template;
+        this.data = data;
+    }
+    
+    mustacheTemplating (){
+        this.template.html();        
+        this.container.append(Mustache.render(this.template, this.data)); 
+    }
+}
+
+class Product{
+    constructor(title, image, desc, price) {
+        this.title = title;
+        this.imageUrl = image;
+        this.description = desc;
+        this.price = price;
+    }
+}
+
+
+class ElementAttribute {
+    constructor(attrName, attrValue){
+        this.name = attrName;
+        this.value = attrValue;
+    }
+}
+
+class Component{
+    constructor(renderHookId){
+        this.hookId = renderHookId;
+    }
+
+    createRootElement(tag, cssClasses, attributes){
+        const rootElement = document.createElement(tag);
+        if(cssClasses){
+            rootElement.className = cssClasses;
+        }
+
+        if(attributes && attributes.length > 0){
+            for (const attr of attributes) {
+                rootElement.setAttribute(attr.name, attr.value)
+            }
+        }
+    }
+}
+
+class ShoppingCart extends Component{
+    items = [];
+
+    set cartItems(value){
+        this.items = value;
+        this.totalOutput.innerHTML = `<h2> Total: \$${this.totalAmount.toFixed(2)}</h2>`;
+    }
+
+    get cartItems(){
+        this.totalOutput;
+    }
+
+    get totalAmount(){
+        const sum = this.amount.reduce(
+            (prevValue, curItem) => prevValue + curItem.price, 0
+        )
+          return sum;
+    }
+
+    constructor(renderHookId){
+        super(renderHookId)
+    }
+
+    addProduct(product){
+        const updatedItems = [...this.items];
+        updatedItems.push(product);
+        this.cartItems = updatedItems;
+    }
+
+    render(){
+        const cartElement = this.createRootElement('section','cart');
+        cartElement.innerHTML = `
+            <h2>Total: \$${0}</h2>
+            <button>Order Now!</button>
+        `;
+        this.totalOutput = carEl.querySelector('h2');
+    }
+}
+
+
+class ProductItem {
+    constructor(product) {
+        this.product = product;
+    }
+
+    addToCart() {
+        App.addProductToCart(this.product);
+    }
+
+    render() {
+        const prodEl = document.createElement('li');
+        prodEl.className = 'product-item';
+        prodEl.innerHTML = `
+            <div>
+                <img src="${this.product.imageUrl}" alt="${this.product.title}" >
+                <div class="product-item__content">
+                <h2>${this.product.title}</h2>
+                <h3>\$${this.product.price}</h3>
+                <p>${this.product.description}</p>
+                <button>Add to Cart</button>
+                </div>
+            </div>
+        `;
+        const addCartButton = prodEl.querySelector('button');
+        addCartButton.addEventListener('click', this.addToCart.bind(this));
+        return prodEl;
+    }
+}
+
+class ProductList {
+    products =[
+        new Product(
+        'A Pillow',
+        'https://www.maxpixel.net/static/photo/2x/Soft-Pillow-Green-Decoration-Deco-Snuggle-1241878.jpg',
+        'A soft pillow!',
+        19.99
+        ),
+        new Product(
+        'A Carpet',
+        'https://upload.wikimedia.org/wikipedia/commons/thumb/7/71/Ardabil_Carpet.jpg/397px-Ardabil_Carpet.jpg',
+        'A carpet which you might like - or not.',
+        89.99
+        )
+    ];
+
+    constructor(){}
+
+    render() {
+        const prodList = document.createElement('ul');
+        prodList.className = 'product-list';
+        for (const prod of this.products) {
+            const productItem = new ProductItem(prod);
+            const prodEl = productItem.render();
+            prodList.append(prodEl);
+        }
+
+        return prodList;
+    }
+}
+
+class Shop{
+    render() {
+        const renderHook = document.getElementById('app');
+
+        this.cart = new ShoppingCart('cart');
+        this.cart.render();
+
+        const productList = new ProductList();
+        const prodListEl = productList.render();
+
+        renderHook.append(prodListEl)
+    }
+}
+
+class App {
+    static cart;
+
+    static init(){
+        const shop = new Shop();
+        shop.render();
+        this.cart = shop.cart;
+    }
+
+    static addProductToCart(product){
+        this.cart.addProduct(product);
+    }
+}
+
+App.init();
+
